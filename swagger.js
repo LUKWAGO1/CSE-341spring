1150ae5b6dafe4be35eeee36f3d92801623f8958
LUKWAGO1 <paullukwago78@gmail.com>
2025-05-20 22:00:33 +0300

    apl

--- swagger.js
+++ swagger.js
@@ -15,26 +15,26 @@ const doc = {
   ],
   definitions: {
     Contact: {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
+      firstName: 'Paul',
+      lastName: 'Lukwago',
+      email: 'paullukwago@gmail.com',
       favoriteColor: 'blue',
-      birthday: '1990-01-01'
+      birthday: '1999-01-01'
     },
     ContactInput: {
       $firstName: 'John',
-      $lastName: 'Doe',
-      $email: 'john.doe@example.com',
+      $lastName: 'daniel',
+      $email: 'johndaniel@gmail.com',
       $favoriteColor: 'blue',
-      $birthday: '1990-01-01'
+      $birthday: '1995-08-01'
     },
     ContactResponse: {
       _id: '682274ddd0a5b29bade88762',
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
+      firstName: 'edward',
+      lastName: 'yawe',
+      email: 'edwardyawe@gmail.com',
       favoriteColor: 'blue',
-      birthday: '1990-01-01'
+      birthday: '2000-01-21'
     },
     NewContactResponse: {
       id: '682274ddd0a5b29bade88762',
